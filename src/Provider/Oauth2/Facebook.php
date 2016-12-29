a34cf3a4237382b6d5f9f1adad5c65914e842664
mirae <mirae@kilikili.idv.tw>
2016-12-29 17:20:20 +0800

    fixed facebook access token url

--- src/Provider/Oauth2/Facebook.php
+++ src/Provider/Oauth2/Facebook.php
@@ -5,10 +5,11 @@ namespace Kilikili\Auth\Client\Provider\Oauth2;
 class Facebook extends AbstractProvider
 {
 	protected $clientAuthUrl = "https://www.facebook.com/v%s/dialog/oauth";
-	protected $clientTokenUrl = "https://www.facebook.com/oauth/access_token";
+	protected $clientTokenUrl = "https://graph.facebook.com/v%s/oauth/access_token";
     
     public function __construct($argu, $custData = array(), $custRData = array()){
         parent::__construct($argu, $custData, $custRData);
         $this->clientAuthUrl = sprintf($this->clientAuthUrl, $custData['version']);
+        $this->clientTokenUrl = sprintf($this->clientTokenUrl, $custData['version']);
     }
 }
